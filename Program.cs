411942be2996a280c99a0dc1a97602e7faa910b6
Ruhama Sut <sutruhama@yahoo.com>
2021-04-26 16:42:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------BUBBLE SORT----------");
+            int[] a = { 4, 10, 0, 6, -3, 5, 1 };
+            int t;
+           
+            Console.WriteLine("Original array :");
+            foreach (int number in a)
+                Console.Write(number + " ");
+            for (int p = 0; p <= a.Length - 2; p++)
+            {
+                
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                        
+                    }
+                }
+                
+            }
+            Console.WriteLine("\n" + "Sorted array :");
+            foreach (int number in a)
+                Console.Write(number + " ");
+            Console.Write("\n");
+
+        }
+    }
+    }
+
